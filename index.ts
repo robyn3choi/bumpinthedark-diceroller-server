30d3541109db32babf8edd459da4ad49cb38136a
Robyn Choi <robyn3choi@gmail.com>
2023-10-23 21:38:12 -0700

    keeper, fixed user leave error, room exist check

--- index.ts
+++ index.ts
@@ -23,7 +23,7 @@ const io = new Server(server, {
   },
 })
 
-const users: { name: string; room: string }[] = []
+const users: { name: string; room: string; isKeeper: boolean; socketId: string }[] = []
 
 app.get('/', (req, res) => {
   res.send('hello world')
@@ -32,14 +32,13 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('connected', socket.id)
 
-  socket.on('userJoined', ({ room, username }) => {
+  socket.on('userJoined', ({ room, username, isKeeper }) => {
     socket.join(room)
-    users.push({ name: username, room })
+    users.push({ name: username, room, isKeeper, socketId: socket.id })
 
-    console.log(`user: ${username} joined room: ${room}`)
+    console.log(`${isKeeper ? 'keeper' : 'hunter'}: ${username} joined room: ${room}`)
 
-    const usersInRoom = users.filter((p) => p.room === room).map((p) => p.name)
-
+    const usersInRoom = users.filter((p) => p.room === room)
     io.to(room).emit('usersUpdated', usersInRoom)
   })
 
@@ -56,31 +55,40 @@ io.on('connection', (socket) => {
     io.sockets.in(room).emit('rolled', { rollType, position, hasDisadvantage, dice, diceCount, username })
   })
 
+  socket.on('clocksUpdated', ({ clocks, room }) => {
+    io.sockets.in(room).emit('clocksUpdated', clocks)
+  })
+
   socket.on('disconnecting', (reason) => {
     let room
     socket.rooms.forEach((r) => {
       if (r !== socket.id) room = r
     })
-    const index = users.findIndex((p) => p.room === room)
+    const index = users.findIndex((u) => u.socketId === socket.id)
     if (index > -1) {
-      users.splice(index, 1)
+      const [removedUser] = users.splice(index, 1)
+      console.log(`Player: ${removedUser.name} with socketId: ${socket.id} left room: ${room}`)
+    } else {
+      console.log(
+        `Tried to remove user with socketId: ${socket.id} from room: ${room}, but it didn't exist in the users array`
+      )
     }
-    io.to(room).emit(
-      'usersUpdated',
-      users.map((p) => p.name)
-    )
+
+    const usersInRoom = users.filter((u) => u.room === room)
+    io.to(room).emit('usersUpdated', usersInRoom)
   })
 })
 
 app.get('/user', (req, res) => {
   const { username, room } = req.query
-  if (users.find((p) => p.room === room && p.name === username)) {
-    res.send({ exists: true })
-  } else {
-    res.send({ exists: false })
-  }
+  res.send({ doesUserExist: !!users.find((u) => u.room === room && u.name === username) })
+})
+
+app.get('/room', (req, res) => {
+  const { room } = req.query
+  res.send({ doesRoomExist: !!users.find((u) => u.room === room) })
 })
 
 server.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`)
+  console.log(`Server running on port ${port}!`)
 })
